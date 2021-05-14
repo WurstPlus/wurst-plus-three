a83b215acc6cbdcd2cfd73e977a72046e6db2fd5
Madmegsox <madmegsox2@gmail.com>
2021-05-14 17:27:43 +0100

    [Addition] Sin color util

--- src/main/java/me/travis/wurstplusthree/util/ColorUtil.java
+++ src/main/java/me/travis/wurstplusthree/util/ColorUtil.java
@@ -19,10 +19,46 @@ public class ColorUtil {
        Color c = Color.getHSBColor((float) (rainbowState / 360.0), 1f, 1f);
        return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
     }
-    /*
-    public static Color getSinColor(final Color c1, final Color c2, final int delay) {
 
+    public static Color getSinState(final Color c1, final double delay, final int a,final type t, final mode m) {
+        double sineState;
+        if(m.equals(mode.ASB)){
+            sineState = Math.abs(Math.sin(2400 - System.currentTimeMillis() / delay)+1);
+        }else {
+            sineState = Math.max(1.0, Math.sin(2400 - System.currentTimeMillis() / delay) + 1);
+        }
+        float[] hsb = Color.RGBtoHSB(c1.getRed(), c1.getGreen(), c1.getBlue(), null);
+        Color c = null;
+        switch (t){
+            case HUE:
+                sineState %= hsb[0];
+                c = Color.getHSBColor((float) sineState, 1f, 1f);
+                break;
+            case SATURATION:
+                sineState %= hsb[1];
+                c = Color.getHSBColor(hsb[0], (float) sineState, 1f);
+                break;
+            case BRIGHTNESS:
+                sineState %= hsb[2];
+                c = Color.getHSBColor(hsb[0], 1f, (float) sineState);
+                break;
+            case SPECIAL:
+                sineState %= hsb[1];
+                c = Color.getHSBColor(hsb[0], 1f, (float) sineState);
+                break;
+        }
+        return new Color(c.getRed(), c.getGreen(), c.getBlue(), a);
     }
-    // max(1, abs(sin x/3)3 - 2.7)-1
-     */
+
+    enum type{
+        HUE,
+        SATURATION,
+        BRIGHTNESS,
+        SPECIAL
+    }
+
+    enum mode{
+        ASB,
+        MAX
+    }
 }
