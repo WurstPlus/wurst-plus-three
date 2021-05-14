55a060f8a2f759c41ca2aa3ba0659d08bb3c4010
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2021-05-14 12:29:51 +0100

    [Addition] started sin color TODO: - finish sin color

--- src/main/java/me/travis/wurstplusthree/util/ColorUtil.java
+++ src/main/java/me/travis/wurstplusthree/util/ColorUtil.java
@@ -19,4 +19,10 @@ public class ColorUtil {
        Color c = Color.getHSBColor((float) (rainbowState / 360.0), 1f, 1f);
        return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
     }
+    /*
+    public static Color getSinColor(final Color c1, final Color c2, final int delay) {
+
+    }
+    // max(1, abs(sin x/3)3)-1
+     */
 }
