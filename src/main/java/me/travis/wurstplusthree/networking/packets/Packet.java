1b90298989ed0f667356f324818f1a533406b5e9
Madmegsox <madmegsox2@gmail.com>
2021-05-16 21:07:59 +0100

    [Addition] stated client packets

---
+++ src/main/java/me/travis/wurstplusthree/networking/packets/Packet.java
@@ -0,0 +1,14 @@
+package me.travis.wurstplusthree.networking.packets;
+
+import java.net.Socket;
+
+/**
+ * @author Madmegsox1
+ * @since 16/05/2021
+ */
+
+public class Packet {
+    public void onCall(Socket socket, String[] packet){
+
+    }
+}
