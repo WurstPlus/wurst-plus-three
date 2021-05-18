ba56bd3c1cdc9e7b7c375a94e26e1c9177b899fb
Madmegsox <madmegsox2@gmail.com>
2021-05-18 16:43:35 +0100

    [Addition] stated client packets

---
+++ src/main/java/me/travis/wurstplusthree/networking/Packet.java
@@ -0,0 +1,21 @@
+package me.travis.wurstplusthree.networking;
+
+import net.minecraft.client.Minecraft;
+
+import java.io.IOException;
+
+/**
+ * @author Madmegsox1
+ * @since 16/05/2021
+ */
+
+public class Packet {
+    protected final Minecraft mc = Minecraft.getMinecraft();
+    public String[] run(String... arguments) throws IOException {
+        return null;
+    }
+
+    public String[] run()throws IOException{
+        return null;
+    }
+}
