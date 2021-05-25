2362f281b5f113b684ba09940cf3a580d8bf6b84
TrvsF <traviseff@gmail.com>
2021-05-25 07:08:04 +0100

    revert to old offhand

--- src/main/java/me/travis/wurstplusthree/hack/combat/Offhand.java
+++ src/main/java/me/travis/wurstplusthree/hack/combat/Offhand.java
@@ -1,22 +1,20 @@
 package me.travis.wurstplusthree.hack.combat;
 
-/**
- * @Author wallhacks0
- * @Author TrvsF
- */
+import me.travis.wurstplusthree.WurstplusThree;
 import me.travis.wurstplusthree.hack.Hack;
 import me.travis.wurstplusthree.setting.type.BooleanSetting;
 import me.travis.wurstplusthree.setting.type.EnumSetting;
 import me.travis.wurstplusthree.setting.type.IntSetting;
+import me.travis.wurstplusthree.setting.type.KeySetting;
 import me.travis.wurstplusthree.util.EntityUtil;
-import net.minecraft.client.gui.inventory.GuiInventory;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityEnderCrystal;
+import me.travis.wurstplusthree.util.MouseUtil;
+import me.travis.wurstplusthree.util.PlayerUtil;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemPickaxe;
-import net.minecraft.item.ItemSword;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import java.util.Arrays;
 
@@ -24,101 +22,98 @@ import java.util.Arrays;
 public class Offhand extends Hack {
 
     EnumSetting mode = new EnumSetting("Mode", "Totem", Arrays.asList("Totem", "Crystal", "Gapple"), this);
-    IntSetting totemHp = new IntSetting("Totem HP", 16, 0, 36, this);
-    IntSetting holeHp = new IntSetting("Hole HP", 16, 0, 36, this);
-    BooleanSetting crystalCheck = new BooleanSetting("CrystalCheck", false, this);
-    IntSetting crystalDistance = new IntSetting("Crystal Distance", 10, 0, 14, this);
-    EnumSetting gapSwap = new EnumSetting("GapSwap", "Never", Arrays.asList("Never", "Sword", "Pickaxe", "Both",  "Always"), this);
-    IntSetting steps = new IntSetting("Steps", 1, 1, 3, this);
+    BooleanSetting gapHole = new BooleanSetting("Gap In Hole", true, this);
+    IntSetting TotemHp = new IntSetting("Totem HP", 16, 0, 36, this);
+    KeySetting gapKey = new KeySetting("Gap Key", Keyboard.KEY_NONE, this);
 
-    private int step;
+    private boolean switching;
+    private boolean keyPressed;
     private int lastSlot;
 
-    public void onEnable() {
-        step = -1;
-    }
-
     @Override
-    public void onTick() {
-        if (mc.currentScreen == null || mc.currentScreen instanceof GuiInventory) {
-            if (step != -1) {
-                swapItems(lastSlot, step);
+    public void onUpdate() {
+
+        if (switching) {
+            swapItems(lastSlot, 2);
+            return;
+
+        }
+        if (mc.currentScreen instanceof GuiContainer) {
+            return;
+
+        }
+
+        float hp = EntityUtil.getHealth(mc.player);
+
+        if (hp < TotemHp.getValue()) {
+            this.swapItems(getItemSlot(Items.TOTEM_OF_UNDYING), 1);
+            return;
+        }
+        if (gapKey.getKey() < -1) {
+            if (Mouse.isButtonDown(MouseUtil.convertToMouse(gapKey.getKey()))) {
+                if (!keyPressed && mc.currentScreen == null) {
+                    this.swapItems(getItemSlot(Items.GOLDEN_APPLE), 1);
+                }
+                keyPressed = true;
                 return;
+            } else {
+                keyPressed = false;
             }
-            float hp = mc.player.getHealth() + mc.player.getAbsorptionAmount();
-            if ((hp <= (EntityUtil.isInHole(mc.player) ? holeHp.getValue() : totemHp.getValue()) || lethalToLocalCheck() || (mode.getValue().equals("Totem") && !shouldGapSwap()))) {
-                swapItems(getItemSlot(Items.TOTEM_OF_UNDYING), steps.getValue());
+        } else if (gapKey.getKey() > -1) {
+            if (Keyboard.isKeyDown(gapKey.getKey())) {
+                if (!keyPressed && mc.currentScreen == null) {
+                    this.swapItems(getItemSlot(Items.GOLDEN_APPLE), 1);
+                }
+                keyPressed = true;
                 return;
-            }
-            if (mode.getValue().equals("Gapple") || shouldGapSwap()) {
-                swapItems(getItemSlot(Items.GOLDEN_APPLE), steps.getValue());
-                return;
-            }
-            if (mode.getValue().equals("Crystal")) {
-                swapItems(getItemSlot(Items.END_CRYSTAL), steps.getValue());
+            } else {
+                keyPressed = false;
             }
         }
+
+
+        if (PlayerUtil.isInHole() && gapHole.getValue() && !WurstplusThree.HACKS.ishackEnabled("Crystal Aura")) {
+            this.swapItems(getItemSlot(Items.GOLDEN_APPLE), 1);
+            return;
+        }
+
+
+
+        switch (mode.getValue()) {
+            case "Totem":
+                this.swapItems(getItemSlot(Items.TOTEM_OF_UNDYING), 1);
+                return;
+            case "Crystal":
+                this.swapItems(getItemSlot(Items.END_CRYSTAL), 1);
+                return;
+            case "Gapple":
+                this.swapItems(getItemSlot(Items.GOLDEN_APPLE), 1);
+                return;
+        }
+
+
     }
 
     public void swapItems(int slot, int step) {
         if (slot == -1) return;
+        if (step == 0) {
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+            mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
+            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
+        }
         if (step == 1) {
             mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
-            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            step = -1;
+            switching = true;
+            lastSlot = slot;
         }
         if (step == 2) {
-            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            this.step = 4;
-            lastSlot = slot;
-        }
-        if (step == 3) {
-            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            this.step = 5;
-            lastSlot = slot;
-        }
-        if (step == 4) {
             mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
             mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            this.step = -1;
+            switching = false;
+
         }
-        if (step == 5) {
-            mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
-            this.step = 6;
-        }
-        if (step == 6) {
-            mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
-            this.step = -1;
-        }
+
         mc.playerController.updateController();
-    }
-
-    private boolean shouldGapSwap() {
-        if (mc.player.getHeldItemMainhand().getItem() != Items.GOLDEN_APPLE && mc.gameSettings.keyBindUseItem.isKeyDown()) {
-            if ((gapSwap.getValue().equals("Sword") || gapSwap.getValue().equals("Both")) && mc.player.getHeldItemMainhand().getItem() instanceof ItemSword) {
-                return true;
-            }
-            if ((gapSwap.getValue().equals("Pickaxe") || gapSwap.getValue().equals("Both")) && mc.player.getHeldItemMainhand().getItem() instanceof ItemPickaxe) {
-                return true;
-            }
-            if (gapSwap.getValue().equals("Always")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean lethalToLocalCheck() {
-        if (!crystalCheck.getValue()) {
-            return false;
-        }
-        for (Entity entity : mc.world.loadedEntityList) {
-            if (entity instanceof EntityEnderCrystal && mc.player.getDistance(entity) <= crystalDistance.getValue()) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private int getItemSlot(Item input) {
