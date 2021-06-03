d6314817aafa192158e3fc8546cb0797ae1cc96d
Madmegsox <madmegsox2@gmail.com>
2021-06-03 21:56:19 +0100

    [improvement] to ca and added ram debug mode

--- src/main/java/me/travis/wurstplusthree/command/commands/Debug.java
+++ src/main/java/me/travis/wurstplusthree/command/commands/Debug.java
@@ -1,5 +1,6 @@
 package me.travis.wurstplusthree.command.commands;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.travis.wurstplusthree.command.Command;
 import me.travis.wurstplusthree.util.ClientMessage;
 import me.travis.wurstplusthree.util.logview.Threads;
@@ -19,6 +20,17 @@ public class Debug extends Command {
         if(message[0].equals("logview")){
             Threads log = new Threads();
             log.start();
+        }
+        else if(message[0].equals("ram")){
+            Runtime runtime = Runtime.getRuntime();
+            long maxMemory = runtime.maxMemory();
+            long allocatedMemory = runtime.totalMemory();
+            long freeMemory = runtime.freeMemory();
+            ClientMessage.sendMessage(
+                    "\n - MAX MEMORY: "+ ChatFormatting.AQUA + maxMemory/1024/1024 + " Mb"+ ChatFormatting.RESET +
+                    "\n - ALLOCATED MEMORY: "+ ChatFormatting.AQUA + allocatedMemory/1024/1024 + " Mb"+ ChatFormatting.RESET +
+                    "\n - FREE MEMORY: " + ChatFormatting.AQUA + freeMemory/1024/1024 +" Mb"    + ChatFormatting.RESET+
+                    "\n - TOTAL FREE MEMORY: " + ChatFormatting.AQUA  + ((freeMemory + (maxMemory - allocatedMemory))/1024/1024)+ " Mb" + ChatFormatting.RESET);
         }
         else {
             ClientMessage.sendErrorMessage(message[0] + " inst supported!");
