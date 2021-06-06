6e6b0b110202c35c6f0f1a2051092a2fcd608d17
Madmegsox <madmegsox2@gmail.com>
2021-06-06 11:11:46 +0100

    [why]

--- src/main/java/me/travis/wurstplusthree/hack/render/Aspect.java
+++ src/main/java/me/travis/wurstplusthree/hack/render/Aspect.java
@@ -1,6 +1,7 @@
 package me.travis.wurstplusthree.hack.render;
 
 import me.travis.wurstplusthree.event.events.PerspectiveEvent;
+import me.travis.wurstplusthree.event.processor.CommitEvent;
 import me.travis.wurstplusthree.hack.Hack;
 import me.travis.wurstplusthree.setting.type.DoubleSetting;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,7 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Aspect extends Hack{
     DoubleSetting aspect = new DoubleSetting("Aspect",  mc.displayWidth / mc.displayHeight + 0.0, 0.0 ,3.0, this);
 
-    @SubscribeEvent
+    @CommitEvent
     public void onPerspectiveEvent(PerspectiveEvent event){
         event.setAspect(aspect.getValue().floatValue());
     }
