123b0a3f26b084543602df52783c079947528d95
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2021-06-09 10:43:02 +0100

    [improvement] EventProcessor

--- src/main/java/me/travis/wurstplusthree/event/processor/EventProcessor.java
+++ src/main/java/me/travis/wurstplusthree/event/processor/EventProcessor.java
@@ -1,7 +1,12 @@
 package me.travis.wurstplusthree.event.processor;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author Madmegsox1
@@ -20,28 +25,20 @@ public final class EventProcessor {
      * @param object the class that the events are in
      * @throws IllegalArgumentException
      */
-    public void addEventListener(Object object) throws IllegalArgumentException {
+    public final void addEventListener(@NotNull Object object) throws IllegalArgumentException {
         getEvents(object);
     }
 
-    public void removeEventListener(Object object) {
-        List<Listener> toRemove = new ArrayList<>();
-        for(Listener listener : events){
-            if(object == listener.object){
-                toRemove.add(listener);
-            }
-        }
-        for (Listener listener : toRemove) {
-            events.remove(listener);
-        }
+    public final void removeEventListener(@NotNull Object object) {
+        events.removeIf(listener -> listener.object == object);
     }
 
     /**
      * @param object takes the class where the events are committed
      */
-    private void getEvents(Object object) {
+    private void getEvents(@NotNull Object object) {
         Class<?> clazz = object.getClass();
-        for (Method method : clazz.getDeclaredMethods()) {
+        Arrays.stream(clazz.getDeclaredMethods()).spliterator().forEachRemaining(method -> {
             if (method.isAnnotationPresent(CommitEvent.class)) {
                 Class<?>[] prams = method.getParameterTypes();
                 if (prams.length != 1) {
@@ -53,30 +50,28 @@ public final class EventProcessor {
                 this.events.add(new Listener(method, object, prams[0], getPriority(method)));
                 this.events.sort(Comparator.comparing(o -> o.priority));
             }
-        }
+        });
     }
 
     /**
      * @param event the event that you are listening for
      * @return if the event was posted or not at a boolean
      */
-    public boolean postEvent(Event event) {
-
-        for (Listener listener : events){
+    public final boolean postEvent(@NotNull Event event) {
+        events.spliterator().forEachRemaining(listener -> {
             if(listener.event == event.getClass()){
                 try {
                     listener.method.setAccessible(true);
                     listener.method.invoke(listener.object, event);
                 } catch (Exception e) {
-                    System.out.println(e);
-                    return false;
+                    e.printStackTrace();
                 }
             }
-        }
+        });
         return true;
     }
 
-    private EventPriority getPriority(Method method) {
+    private EventPriority getPriority(@NotNull Method method) {
         return method.getAnnotation(CommitEvent.class).priority();
     }
 }
